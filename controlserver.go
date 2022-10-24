a2c516dc20fe0ec80d13431c905d7eee21698ca0
harryzhu <harry.chuh@gmail.com>
2022-10-24 23:27:50 +0800

    -

---
+++ controlserver.go
@@ -0,0 +1,45 @@
+package sqlconf
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+	//"fmt"
+	//"go.uber.org/zap"
+	//"golang.org/x/net/http2"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404 Not Found"))
+	}
+
+}
+
+func RemoteShutdownHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusGone)
+	w.Write([]byte("shutdown the server in 3 seconds..."))
+	go func() {
+		time.Sleep(3 * time.Second)
+		os.Exit(0)
+	}()
+
+}
+
+func (h2s *H2Server) runControlServer() {
+	addr := strings.Join([]string{h2s.IP, strconv.Itoa(h2s.Port + 1)}, ":")
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", IndexHandler)
+	mux.HandleFunc("/remote-shutdown", RemoteShutdownHandler)
+
+	http.ListenAndServe(addr, mux)
+}
--- http2server.go
+++ http2server.go
@@ -1,8 +1,18 @@
 package sqlconf
 
 import (
+	//"io/ioutil"
+	//"mime"
 	"net/http"
+	//"os"
+
+	//"path/filepath"
+	"strconv"
+	"strings"
+
 	//"time"
+	//"fmt"
+	"sync"
 
 	"go.uber.org/zap"
 	"golang.org/x/net/http2"
@@ -10,7 +20,8 @@ import (
 
 type H2Server struct {
 	StaticRootDir string
-	Address       string
+	IP            string
+	Port          int
 	TLScert       string
 	TLSkey        string
 }
@@ -22,8 +33,13 @@ func (h2s *H2Server) WithStaticRootDir(s string) *H2Server {
 	return h2s
 }
 
-func (h2s *H2Server) WithAddress(s string) *H2Server {
-	h2s.Address = s
+func (h2s *H2Server) WithIP(s string) *H2Server {
+	h2s.IP = s
+	return h2s
+}
+
+func (h2s *H2Server) WithPort(i int) *H2Server {
+	h2s.Port = i
 	return h2s
 }
 
@@ -33,14 +49,44 @@ func (h2s *H2Server) WithTLS(c, k string) *H2Server {
 	return h2s
 }
 
-func (h2s *H2Server) StartServer() {
+// func (hh *h2sHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path == "/remote-shutdown/bbc" {
+// 		fmt.Fprintf(w, "shutdown")
+// 	} else {
+// 		fullPath := filepath.Join(h2server.StaticRootDir, r.URL.Path)
+// 		zapLogger.Info("file", zap.String("path", fullPath))
+// 		_, err := os.Stat(fullPath)
+// 		if err != nil {
+// 			fmt.Fprintf(w, "404 page not found")
+// 		}
+// 		f, err := os.Open(fullPath)
+// 		if err != nil {
+// 			fmt.Fprintf(w, "404 page not found(open)")
+// 		}
+// 		defer f.Close()
+
+// 		fd, err := ioutil.ReadFile(fullPath)
+// 		if err != nil {
+// 			fmt.Fprintf(w, "404 page not found(read)")
+// 		}
+// 		fmime := mime.TypeByExtension(filepath.Ext(fullPath))
+// 		fmt.Println("mime:", fmime)
+// 		w.Header().Set("Content-Type", fmime)
+
+// 		w.Write(fd)
+// 	}
+// 	w.Header().Set("Content-Type", "text/html")
+// 	fmt.Fprintf(w, "OK")
+// }
+
+func (h2s *H2Server) runH2Server() {
 
 	if h2s.StaticRootDir == "" {
 		h2s.StaticRootDir = "./"
 	}
 
-	if h2s.Address == "" {
-		h2s.Address = ":8080"
+	if h2s.Port <= 0 {
+		h2s.Port = 8080
 	}
 
 	if h2s.TLScert == "" {
@@ -52,17 +98,33 @@ func (h2s *H2Server) StartServer() {
 	}
 
 	server := http.Server{
-		Addr:    h2s.Address,
+		Addr:    strings.Join([]string{h2s.IP, strconv.Itoa(h2s.Port)}, ":"),
 		Handler: http.FileServer(http.Dir(h2s.StaticRootDir)),
 	}
 
 	zapLogger.Info("http2 server",
 		zap.String("StaticRootDir", h2s.StaticRootDir),
-		zap.String("Address", h2s.Address),
+		zap.Int("Port", h2s.Port),
 		zap.String("TLScert", h2s.TLScert),
 		zap.String("TLSkey", h2s.TLSkey),
 	)
 
 	http2.ConfigureServer(&server, &http2.Server{})
+
 	server.ListenAndServeTLS(h2s.TLScert, h2s.TLSkey)
+
+}
+
+func (h2s *H2Server) StartServer() {
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		h2s.runH2Server()
+	}()
+
+	go func() {
+		h2s.runControlServer()
+	}()
+
+	wg.Wait()
 }
